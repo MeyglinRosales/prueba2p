e3d0a20c72fd0c3df2ced66ad154020cb694466f
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-04-21 00:56:44 -0600

    rama4

--- segundop/main.cpp
+++ segundop/main.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Hello world, rstamos probando esto!" << endl;
+    oid consultas();
+void gestionEmpleados();
+void contabilidad();
+void seguridad();
+void bitacoramenu();
+
+
+//Inicio de main
+int main(int argc, char *argv[])
+{
+    Puesto gesPuesto;
+    Bitacora control;
+
+    //Llamado a clase Usuarios
     return 0;
 }
