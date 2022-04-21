950792389e7b4e63e55a890634fe66e37dce8947
MeyglinRosales <mrosaleso3@miumg.edu.gt>
2022-04-21 07:19:07 -0600

    se ha agregado 21mey

--- 21mey/main.cpp
+++ 21mey/main.cpp
@@ -6,7 +6,4 @@ int main()
 {
     cout << "Hello world!" << endl;
     return 0;
-    cout << "Hello world!" << endl;
-    return 0;
-
 }
---
+++ parc2/include/rrr.h
@@ -0,0 +1,16 @@
+#ifndef RRR_H
+#define RRR_H
+
+
+class rrr
+{
+    public:
+        rrr();
+        virtual ~rrr();
+
+    protected:
+
+    private:
+};
+
+#endif // RRR_H
